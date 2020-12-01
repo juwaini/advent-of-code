3a3bd50a1a641cba0465ebcab892393f70398f0f
Juwaini <juwaini@gmail.com>
2020-12-01 15:14:42 +0800

    finished day one

---
+++ day01.py
@@ -0,0 +1,20 @@
+def day_01():
+    with open('input.txt') as input:
+        ls = [int(i) for i in input]
+
+        for x in range(len(ls)):
+            for y in range(x + 1, len(ls)):
+                if ls[x] + ls[y] == 2020:
+                    print(ls[x] * ls[y])
+                    break
+
+        for x in range(len(ls)):
+            for y in range(x + 1, len(ls)):
+                for z in range(y + 1, len(ls)):
+                    if ls[x] + ls[y] + ls[z] == 2020:
+                        print(ls[x] * ls[y] * ls[z])
+                        break
+
+
+if __name__ == '__main__':
+    day_01()
